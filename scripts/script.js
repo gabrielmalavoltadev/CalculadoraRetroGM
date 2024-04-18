c0864313ed03e2c5b1d1e87fb8ef85c4380c9142
gabrielmalavoltadev <gabriel.malavoltawork@gmail.com>
2024-04-17 23:22:39 -0300

    Ajustado alguns pontos de layout e teste de função para botões

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let clique = window.document.querySelector('div.white');
+//clique.addEventListener('click', clicar);
+
+
+function clicar(btn) {
+    document.getElementById('result').innerHTML = btn;
+
+
+}
