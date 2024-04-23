ed38ce368ecaf355f3c770f9fef7b588067841dc
gabrielmalavoltadev <gabriel.malavoltawork@gmail.com>
2024-04-22 22:37:48 -0300

    Adicionado chamada de função em alguns botões e início de lógica de calculo

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
-let clique = window.document.querySelector('div.white');
+var clique = '';
 //clique.addEventListener('click', clicar);
 
 
 function clicar(btn) {
     document.getElementById('result').innerHTML = btn;
-
-
+    clique += btn;
 }
+
+function mostrar() {
+    document.getElementById('result').innerHTML = eval(clique);
+    clique = '';
+}
+
