8ac1fa3ef6467c086569b16404b879d61b28fb84
gabrielmalavoltadev <gabriel.malavoltawork@gmail.com>
2024-05-16 23:40:38 -0300

    Ajustes de CSS e funcionalidades

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
-var clique = '';
+let operacao = '';
 //clique.addEventListener('click', clicar);
 
 
+function limparTela() {
+    document.getElementById('result').innerHTML = '';
+}
+
 function clicar(btn) {
-    document.getElementById('result').innerHTML = btn;
-    clique += btn;
+    document.getElementById('result').innerHTML += btn;
+    operacao += btn;
 }
 
 function mostrar() {
-    document.getElementById('result').innerHTML = eval(clique);
-    clique = '';
+    document.getElementById('result').innerHTML = eval(operacao);
+    operacao = '';
 }
 
